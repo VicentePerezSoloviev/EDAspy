62701dfc3c21fd90d010016b42d0b370ab6478cf
Vicente Pérez Soloviev <vicente.perez.soloviev@gmail.com>
2020-06-02 17:13:37 +0200

    add __init__.py to directories

--- EDApy/__init__.py
+++ EDApy/__init__.py
@@ -4,10 +4,12 @@
 # __init__.py
 
 import sys
+
 if sys.version_info[0] < 3:
     raise Exception("Python version should be greater than 3")
 
 import warnings
+
 warnings.simplefilter(action='ignore', category=FutureWarning)
 from sklearn.cluster import KMeans
 
@@ -16,4 +18,3 @@ from .optimization.univariate.discrete import UMDAd as EDA_discrete
 from .optimization.univariate.continuous import UMDAc as EDA_continuous
 
 from .optimization.multivariate.__BayesianNetwork import print_structure as print_structure
-
