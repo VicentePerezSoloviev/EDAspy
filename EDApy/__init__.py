654463edb12d917e5a5e0aa0781e533c326b5f2c
Vicente Pérez Soloviev <vicente.perez.soloviev@gmail.com>
2020-06-02 16:35:34 +0200

    check py version in __init__.py

--- EDApy/__init__.py
+++ EDApy/__init__.py
@@ -3,6 +3,10 @@
 
 # __init__.py
 
+import sys
+if sys.version_info[0] < 3:
+    raise Exception("Python version should be greater than 3")
+
 import warnings
 warnings.simplefilter(action='ignore', category=FutureWarning)
 from sklearn.cluster import KMeans
