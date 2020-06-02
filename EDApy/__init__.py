386fe4b1543853dd60b6f2c05e7f817d93ed3423
Vicente Pérez Soloviev <vicente.perez.soloviev@gmail.com>
2020-06-02 17:29:48 +0200

    add __init__.py to directories

--- EDApy/__init__.py
+++ EDApy/__init__.py
@@ -13,8 +13,3 @@ import warnings
 warnings.simplefilter(action='ignore', category=FutureWarning)
 from sklearn.cluster import KMeans
 
-from .optimization.multivariate.EDA_multivariate import EDAgbn as EDA_multivariate
-from .optimization.univariate.discrete import UMDAd as EDA_discrete
-from .optimization.univariate.continuous import UMDAc as EDA_continuous
-
-from .optimization.multivariate.__BayesianNetwork import print_structure as print_structure
