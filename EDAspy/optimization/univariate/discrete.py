58cd46426385a7a0f4fcf92131216fa2d67ea33a
Vicente P. Soloviev <vicente.perez.soloviev@gmail.com>
2022-05-13 11:22:47 +0200

    UMDAd optimization

--- EDAspy/optimization/univariate/discrete.py
+++ EDAspy/optimization/univariate/discrete.py
@@ -58,125 +58,70 @@ class UMDAd:
     best_MAE_global = -1
     best_ind_global = -1
 
-    # init function
-    def __init__(self, MAX_IT, DEAD_ITER, SIZE_GEN, ALPHA, vector, cost_function, aim):
+    def __init__(self, size_gen, max_iter, dead_iter, alpha, vector, std_bound=0.3):
         """
-        Constructor of the optimizer class.
+        Constructor of the UMDAc optimizer class
 
-        :param SIZE_GEN: total size of the generations in the execution of the algorithm
-        :type SIZE_GEN: int
-        :param MAX_IT: total number of iterations in case that optimum is not yet found. If reached, the optimum found is returned
-        :type MAX_IT: int
-        :param DEAD_ITER: total number of iteration with no better solution found. If reached, the optimum found is returned
-        :type DEAD_ITER: int
-        :param ALPHA: percentage of the generation tu take, in order to sample from them. The best individuals selection
-        :type ALPHA: float [0-1]
-        :param vector: vector of normal distributions to sample from
-        :type vector: pandas dataframe with columns ['mu', 'std'] and optional ['max', 'min']
-        :param aim: 'minimize' or 'maximize'. Represents the optimization aim.
-        :type aim: string ['minimize' or 'maximize']
-        :param cost_function: cost function to minimize
-        :type cost_function: callable function which receives a dictionary as input and returns a numeric value
-
-        :raises Exception: cost function is not callable
+        Parameters
+        ----------
+            size_gen : int
+                Population size of the algorithm (number of individuals per generation)
+            max_iter : int
+                Maximum number of iterations
+            dead_iter : int
+                Number of iterations after which, if no improvement is found, the runtime finish
+            alpha : float
+                Percentage ([0, 1]) of the population considered for the elite selection
+            vector : list
+                Array with independent probabilities for each variable: P(x_0=1), P(x_1=1), ..., P(x_n=1)
         """
 
-        self.ALPHA = ALPHA
-        self.SIZE_GEN = SIZE_GEN
-        self.MAX_IT = MAX_IT
-
+        self.SIZE_GEN = size_gen
+        self.MAX_ITER = max_iter
+        self.alpha = alpha
         self.vector = vector
-        self.variables = list(vector.columns)
 
-        # check if cost_function is real
-        if callable(cost_function):
-            self.cost_function = cost_function
-        else:
-            raise Exception('ERROR setting cost function. The cost function must be a callable function')
-
-        if aim == 'minimize':
-            self.aim = 'min'
-            self.best_MAE_global = 9999999999
-        elif aim == 'maximize':
-            self.aim = 'max'
-            self.best_MAE_global = -9999999999
-        else:
-            raise Exception('ERROR when setting aim of optimizer. Only "minimize" or "maximize" is possible')
+        self.variables = list(range(len(vector)))
+        self.best_mae_global = 999999999999
 
         # self.DEAD_ITER must be fewer than MAX_ITER
-        if DEAD_ITER >= MAX_IT:
-            raise Exception(
-                'ERROR setting DEAD_ITER. The dead iterations must be fewer than the maximum iterations')
+        if dead_iter > max_iter:
+            raise Exception('ERROR setting DEAD_ITER. The dead iterations must be fewer than the maximum iterations')
         else:
-            self.DEAD_ITER = DEAD_ITER
+            self.DEAD_ITER = dead_iter
 
-    # new individual
-    def __new_individual__(self):
-        """Sample a new individual from the vector of probabilities.
+        self.truncation_length = int(size_gen * alpha)
 
-        :return: a dictionary with the new individual; with names of the parameters as keys and the values.
-        :rtype: dict
-        """
+        # initialization of generation
+        self.generation = np.random.uniform(low=0, high=1, size=(self.SIZE_GEN, len(self.variables)))
+        self.generation = pd.DataFrame(self.generation < [self.vector] * self.SIZE_GEN,
+                                       dtype=int, columns=self.variables)
 
-        num_vars = len(self.variables)
-        sample = list(np.random.uniform(low=0, high=1, size=num_vars))
-        individual = {}
-        index = 0
-        for ind in self.variables:
-            if float(self.vector[ind]) >= sample[index]:
-                individual[ind] = 1
-            else:
-                individual[ind] = 0
-            index = index + 1
-        return individual
+        self.std_bound = std_bound
 
     # new generation
     def new_generation(self):
         """Build a new generation sampled from the vector of probabilities and updates the generation pandas dataframe
         """
-        gen = pd.DataFrame(columns=self.variables)
-
-        while len(gen) < self.SIZE_GEN:
-            individual = self.__new_individual__()
-            gen = gen.append(individual, True)
-
-        self.generation = gen
+        self.generation = np.random.uniform(low=0, high=1, size=(self.SIZE_GEN, len(self.variables)))
+        self.generation = pd.DataFrame(self.generation < [self.vector] * self.SIZE_GEN,
+                                       dtype=int, columns=self.variables)
 
     # check the cost of each individual of the generation
-    def __check_individual__(self, individual):
-        """Check the cost of the individual in the cost function
-        :param individual: dictionary with the parameters to optimize as keys and the value as values of the keys
-        :type individual: dict
-        :return: a cost evaluated in the cost function to optimize.
-        :rtype: float
-        """
-
-        cost = self.cost_function(individual)
-        return cost
-
-    # check the cost of each individual of the generation
-    def check_generation(self):
+    def check_generation(self, objective_function):
         """Check the cost of each individual in the cost function implemented by the user
         """
 
-        for ind in range(len(self.generation)):
-            cost = self.__check_individual__(self.generation.loc[ind])
-            self.generation.loc[ind, 'cost'] = cost
+        self.generation['cost'] = self.generation.apply(lambda row: objective_function(row[self.variables].to_list()),
+                                                        axis=1)
 
-    # selection of the best individuals to mutate the next gen
-    def individuals_selection(self):
-        """Selection of the best individuals of the actual generation and updates the generation by selecting the best
-        individuals
+    # truncate the generation at alpha percent
+    def truncation(self):
+        """
+        Selection of the best individuals of the actual generation.
         """
 
-        length = int(len(self.generation)*self.ALPHA)
-        if self.aim == 'min':
-            self.generation = self.generation.nsmallest(length, 'cost')
-        else:
-            self.generation = self.generation.nlargest(length, 'cost')
-
-        self.generation = self.generation.reset_index()
-        del self.generation['index']
+        self.generation = self.generation.nsmallest(self.truncation_length, 'cost')
 
     # based on the best individuals of the selection, rebuild the prob vector
     def update_vector(self):
@@ -184,67 +129,57 @@ class UMDAd:
          it and update the vector of probabilities
         """
 
-        for ind in self.variables:
-            total = self.generation[ind].sum()
-            prob = total / len(self.generation)
-            self.vector[ind] = prob
+        self.vector = np.array(self.generation.drop('cost', axis=1).sum()) / len(self.generation)
+        for i in range(len(self.vector)):
+            if self.vector[i] < self.std_bound:
+                self.vector[i] = self.std_bound
 
-    # intern function to compare local cost with global one
-    def __compare_costs__(self, local):
-        """Check if the local best cost is better than the global one
-        :param local: local best cost
-        :type local: float
-        :return: True if is better, False if not
-        :rtype: bool
+    # run the class to find the optimum
+    def optimize(self, cost_function, output=True):
+        """
+        Run method to execute the EDA algorithm over a cost function.
+
+        Parameters
+        -------------
+
+        cost_function : callable
+            Cost function to be optimized which accepts a list of values as argument (values to be optimized).
+        output : boolean
+            True if desired to print information during runtime. False otherwise.
+
+        Returns
+        ------------
+
+        list : List of optimum values found after optimization
+        float : Cost of the optimum solution found
+        int : Number of iterations until convergence
         """
 
-        if self.aim == 'min':
-            return local < self.best_MAE_global
-        else:
-            return local > self.best_MAE_global
-
-    # run method
-    def run(self, output=True):
-        """Run method to execute the EDA algorithm
-
-        :param output: True if wanted to print each iteration
-        :type output: bool
-        :return: best cost, best individual, history of costs along execution
-        :rtype: float, pandas dataframe, list
-        """
-
-        dead_iter = 0
-        for i in range(self.MAX_IT):
-            self.new_generation()
-            self.check_generation()
-            self.individuals_selection()
+        not_better = 0
+        for i in range(self.MAX_ITER):
+            self.check_generation(cost_function)
+            self.truncation()
             self.update_vector()
 
-            if self.aim == 'min':
-                best_mae_local = self.generation['cost'].min()
-            else:
-                best_mae_local = self.generation['cost'].max()
+            best_mae_local = self.generation['cost'].min()
 
             self.history.append(best_mae_local)
-            best_ind_local = []
-            best = self.generation[self.generation['cost'] == best_mae_local].loc[0]
+            best_ind_local = self.generation[self.generation['cost'] == best_mae_local]
 
-            for j in self.variables:
-                if int(best[j]) == 1:
-                    best_ind_local.append(j)
-
-            # update best of model
-            if self.__compare_costs__(best_mae_local):
-                self.best_MAE_global = best_mae_local
+            # update the best values ever
+            if best_mae_local < self.best_mae_global:
+                self.best_mae_global = best_mae_local
                 self.best_ind_global = best_ind_local
-                dead_iter = 0
+                not_better = 0
             else:
-                dead_iter = dead_iter + 1
-                if dead_iter == self.DEAD_ITER:
-                    return self.best_MAE_global, self.best_ind_global, self.history
+                not_better += 1
+                if not_better == self.DEAD_ITER:
+                    return self.best_ind_global.reset_index(drop=True).loc[0].to_list()[:-1], self.best_mae_global, \
+                           len(self.history)
+
+            self.new_generation()
 
             if output:
-                print('IT ', i, 'best cost ', best_mae_local)
-                print(best_ind_local)
+                print('IT ', i, 'best cost ', self.best_mae_global)
 
-        return self.best_MAE_global, self.best_ind_global, self.history
+        return self.best_ind_global.reset_index(drop=True).loc[0].to_list()[:-1], self.best_mae_global, len(self.history)
