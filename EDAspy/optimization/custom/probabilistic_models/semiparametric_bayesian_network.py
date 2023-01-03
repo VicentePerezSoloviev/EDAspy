ce2524afd26aa08309d0335b01705aa23759e9e8
Vicente P. Soloviev <vicente.perez.soloviev@gmail.com>
2023-01-03 10:55:03 +0100

    SPEDA + test

---
+++ EDAspy/optimization/custom/probabilistic_models/semiparametric_bayesian_network.py
@@ -0,0 +1,85 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+import numpy as np
+from pybnesian import SemiparametricBN, hc
+from ._probabilistic_model import ProbabilisticModel
+import pandas as pd
+
+
+class SPBN(ProbabilisticModel):
+
+    """
+    This probabilistic model is a Semiparametric Bayesian network [1]. It allows dependencies between variables
+    which have been estimated using KDE with variables which fit a Gaussian distribution.
+
+    References:
+
+        [1]: Atienza, D., Bielza, C., & Larrañaga, P. (2022). PyBNesian: an extensible Python package
+        for Bayesian networks. Neurocomputing, 504, 204-209.
+    """
+
+    def __init__(self, variables: list, white_list: list = None, black_list: list = None):
+        """
+        :param variables: Number of variables
+        :param white_list: List of tuples with mandatory arcs in the BN structure
+        :param black_list: List of tuples with forbidden arcs in the BN structure
+        """
+
+        super().__init__(variables)
+
+        self.variables = variables
+        self.pm = SemiparametricBN(variables)
+
+        self.white_list = white_list
+        self.black_list = black_list
+
+        self.id = 5
+
+    def learn(self, dataset: np.array):
+        """
+        Learn a semiparametric Bayesian network from the dataset passed as argument.
+
+        :param dataset: dataset from which learn the GBN.
+        """
+
+        self.pm = SemiparametricBN(self.variables)
+
+        if self.white_list and self.black_list:
+            self.pm = self.pm = hc(pd.DataFrame(dataset), start=self.pm, operators=["arcs", "node_type"],
+                                   arc_whitelist=self.white_list, arc_blacklist=self.black_list)
+        elif self.white_list:
+            self.pm = self.pm = hc(pd.DataFrame(dataset), start=self.pm, operators=["arcs", "node_type"],
+                                   arc_whitelist=self.white_list)
+        elif self.black_list:
+            self.pm = self.pm = hc(pd.DataFrame(dataset), start=self.pm, operators=["arcs", "node_type"],
+                                   arc_blacklist=self.black_list)
+        else:
+            self.pm = self.pm = hc(pd.DataFrame(dataset), start=self.pm, operators=["arcs", "node_type"])
+
+        self.pm.fit(pd.DataFrame(dataset))
+
+    def print_structure(self) -> list:
+        """
+        Prints the arcs between the nodes that represent the variables in the dataset. This function
+        must be used after the learning process.
+
+        :return: list of arcs between variables
+        :rtype: list
+        """
+
+        return self.pm.arcs()
+
+    def sample(self, size: int) -> np.array:
+        """
+        Samples the Gaussian Bayesian network several times defined by the user. The dataset is returned
+        as a numpy matrix. The sampling process is implemented using probabilistic logic sampling.
+
+        :param size: number of samplings of the Gaussian Bayesian network.
+        :return: array with the dataset sampled.
+        :rtype: np.array
+        """
+
+        dataset = self.pm.sample(size).to_pandas()
+        dataset = dataset[self.variables].to_numpy()
+        return dataset
---
+++ EDAspy/optimization/multivariate/speda.py
@@ -0,0 +1,105 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+from ..eda import EDA
+from ..custom.probabilistic_models import SPBN
+from ..custom.initialization_models import UniformGenInit
+
+import numpy as np
+
+
+class SPEDA(EDA):
+    """
+    Semiparametric Estimation of Distribution Algorithm [1]. This type of Estimation-of-Distribution
+    Algorithm uses a semiparametric Bayesian network which allows dependencies between variables
+    which have been estimated using KDE with variables which fits a Gaussian distribution. By this
+    way, it avoid the assumption of Gaussianity in the variables of the optimization problem. This
+    multivariate probabilistic model is updated in each iteration with the best individuals of the
+    previous generation.
+
+    SPEDA has shown to improve the results for more complex optimization problem compared to the
+    univariate EDAs that can be found implemented in this package, multivariate EDAs such as
+    EGNA, or EMNA, and other population-based algorithms. See [1] for numerical results.
+
+    Example:
+
+        This example uses some very well-known benchmarks from CEC14 conference to be solved using
+        a Semiparametric Estimation of Distribution Algorithm (SPEDA).
+
+        .. code-block:: python
+
+            from EDAspy.optimization import SPEDA
+            from EDAspy.benchmarks import ContinuousBenchmarkingCEC14
+
+            benchmarking = ContinuousBenchmarkingCEC14(10)
+
+            speda = SPEDA(size_gen=300, max_iter=100, dead_iter=20, n_variables=10,
+                          landscape_bounds=(-60, 60), l=10)
+
+            eda_result = speda.minimize(benchmarking.cec14_4, True)
+
+    References:
+
+        [1]: Vicente P. Soloviev, Concha Bielza and Pedro Larrañaga. Semiparametric Estimation
+        of Distribution Algorithm for continuous optimization. 2022
+
+    """
+
+    def __init__(self,
+                 size_gen: int,
+                 max_iter: int,
+                 dead_iter: int,
+                 n_variables: int,
+                 landscape_bounds: tuple,
+                 l: float,
+                 alpha: float = 0.5,
+                 disp: bool = True,
+                 black_list: list = None,
+                 white_list: list = None):
+        r"""
+            :param size_gen: Population size. Number of individuals in each generation.
+            :param max_iter: Maximum number of iterations during runtime.
+            :param dead_iter: Stopping criteria. Number of iterations with no improvement after which, the algorithm finish.
+            :param n_variables: Number of variables to be optimized.
+            :param landscape_bounds: Landscape bounds only for initialization. Limits in the search space.
+            :param alpha: Percentage of population selected to update the probabilistic model.
+            :param l: SPEDA is an archive-base approach. Thus, in each generation updates the probabilistic model with
+            the best solutions of the previous l generations.
+            :param alpha: Percentage of population selected to update the probabilistic model in each generation.
+            :param disp: Set to True to print convergence messages.
+            :param black_list: list of tuples with the forbidden arcs in the GBN during runtime.
+            :param white_list: list of tuples with the mandatory arcs in the GBN during runtime.
+        """
+
+        super().__init__(size_gen=size_gen, max_iter=max_iter, dead_iter=dead_iter,
+                         n_variables=n_variables, alpha=alpha, elite_factor=alpha, disp=disp)
+
+        self.vars = [str(i) for i in range(n_variables)]
+        self.landscape_bounds = landscape_bounds
+        self.pm = SPBN(self.vars, black_list=black_list, white_list=white_list)
+
+        self.l_len = l*int(size_gen*self.alpha)  # maximum number of individuals in the archive
+        self.archive = np.empty((0, self.n_variables))
+
+        # In this implementation the individuals of the first generation are sampled from a uniform distribution
+        # to not skew the following estimation of distributions.
+
+        self.init = UniformGenInit(self.n_variables, lower_bound=self.landscape_bounds[0],
+                                   upper_bound=self.landscape_bounds[1])
+
+    def _update_archive(self):
+        self.archive = np.append(self.archive, self.elite_temp, axis=0)
+        self.archive = self.archive[-self.l_len:]
+
+    def _update_pm(self):
+        """
+        Learn the probabilistic model from the best individuals of previous generation, using the best solutions
+        of the previous l generations
+        """
+        self._update_archive()
+        self.pm.learn(dataset=self.archive)
+
+    def _new_generation(self):
+        self.generation = np.concatenate([self.pm.sample(size=self.size_gen), [self.best_ind_global]])
+        # as it is not an elitist approach we just add the best individual to show always an improvement in the
+        # history of the best solution costs
